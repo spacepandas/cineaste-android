9e73c24cc12120663f156f48eec9b83231f54b0d
Marcel Groß <mgr@adorsys.de>
2016-01-19 20:36:00 +0100

    removed api-keys from repo

--- app/src/main/java/de/cineaste/android/Constants.java
+++ app/src/main/java/de/cineaste/android/Constants.java
@@ -5,6 +5,6 @@ public interface Constants {
     int DATABASE_VERSION = 1;
     String DATABASE_NAME = "cineaste.db";
 
-    String API_KEY = "7ddd4ae84251b937ce8cc6a9064d7d10";
+    String API_KEY = BuildConfig.MovieKey;
     String POSTER_URI = "https://image.tmdb.org/t/p/w185<posterName>?api_key=" + API_KEY;
 }
