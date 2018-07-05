9335670cf0bfdd3ef66638a43494667f8d068b0c
Marcel Groß <marcel.gross@kurzdigital.com>
2018-07-05 18:50:44 +0200

    Change about page to html page

--- app/src/main/kotlin/de/cineaste/android/activity/AboutActivity.kt
+++ app/src/main/kotlin/de/cineaste/android/activity/AboutActivity.kt
@@ -1,31 +1,31 @@
 package de.cineaste.android.activity
 
-import android.content.Intent
-import android.net.Uri
+import android.annotation.SuppressLint
+import android.content.res.Resources
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
 import de.cineaste.android.BuildConfig
 import de.cineaste.android.R
 
-class AboutActivity : AppCompatActivity(), View.OnClickListener {
+class AboutActivity : AppCompatActivity() {
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
 
         initToolbar()
 
-        val movieDbLogo = findViewById<ImageView>(R.id.themoviedb_logo)
-
-        movieDbLogo.setOnClickListener(this)
-
-        val version = findViewById<TextView>(R.id.version)
-        version.text = resources.getString(R.string.version, BuildConfig.VERSION_NAME)
+        val webView = findViewById<WebView>(R.id.webview)
+        val webSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webView.addJavascriptInterface(WebInterface(resources), "Android")
+        webView.loadUrl("file:///android_res/raw/about.html")
     }
+
 
     private fun initToolbar() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
@@ -40,20 +40,13 @@ class AboutActivity : AppCompatActivity(), View.OnClickListener {
         onBackPressed()
         return super.onSupportNavigateUp()
     }
+}
 
-    private fun openWebsite(url: String) {
-        val intent = Intent(Intent.ACTION_VIEW)
-        intent.data = Uri.parse(url)
-        startActivity(intent)
-    }
+class WebInterface(private val resources: Resources) {
 
-    override fun onClick(v: View) {
-        when (v.id) {
-            R.id.themoviedb_logo -> openWebsite(MOVIE_DB_URL)
-        }
-    }
-
-    companion object {
-        private const val MOVIE_DB_URL = "https://www.themoviedb.org/"
+    @Suppress("unused")
+    @JavascriptInterface
+    fun addVersion(): String {
+        return resources.getString(R.string.version, BuildConfig.VERSION_NAME)
     }
 }
